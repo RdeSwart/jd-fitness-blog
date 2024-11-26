0d4df08a4eeebd8897e1fa0673a35375baf9ba60
Ruth De Swart <ruthdeswart@outlook.ie>
2024-11-26 16:11:52 +0000

    feat: Install & wire up SummerNote for better use of admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 from .models import BlogPost, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
 
 #Allows you to CRUD blog posts from admin panel
-admin.site.register(BlogPost)
+@admin.register(BlogPost)
+class PostAdmin(SummernoteModelAdmin):
+    """
+    Using SummerNote, custom the admin config to manage
+    BlogPost objects in the admin section of the site
+    """
+    list_display = ('title','slug','status')
+    search_fields = ['title','content']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content')
 
 #Allows you to approve comments and customises the data
 admin.site.register(Comment)
--- jdfitness/urls.py
+++ jdfitness/urls.py
@@ -18,7 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 #from blog.views import my_blog
 
+
+# Put in alphabetical order
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('summernote/',include('django_summernote.urls')),
     path('',include("blog.urls"), name='blog-urls'),
 ]
