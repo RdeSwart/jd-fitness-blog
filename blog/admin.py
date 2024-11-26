fbc14fdb1ec66d8bac19893882e8476ea60744ee
Ruth De Swart <ruthdeswart@outlook.ie>
2024-11-26 11:52:34 +0000

    feat: Create BlogPost model, register it and add CSRF origins

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import BlogPost
 
 # Register your models here.
+
+#Allows you to CRUD blog posts from admin panel
+admin.site.register(BlogPost)
--- jdfitness/settings.py
+++ jdfitness/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9c)*a+x#&xa$^edir&cra#**e)y$ybn&bz#_het5xe4@5&$er-'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -90,6 +90,11 @@ WSGI_APPLICATION = 'jdfitness.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 
 # Password validation
