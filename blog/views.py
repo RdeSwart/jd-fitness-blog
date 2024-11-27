3a12422bfd328db9b9bd2e37605410cfc852c9c0
Ruth De Swart <ruthdeswart@outlook.ie>
2024-11-27 10:43:35 +0000

    feat: Create home page and add CSS styling and url links

--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,5 @@ from .models import BlogPost
 # Create your views here.
 class BlogDetail(generic.ListView):
     queryset = BlogPost.objects.all()
-    template_name = "blogpost_list.html"
+    template_name = "blog/index.html"
+    paginate_by = 6
