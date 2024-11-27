f69391e3d6f812cfe96bf8684a9d31c0e9d1a256
Ruth De Swart <ruthdeswart@outlook.ie>
2024-11-27 15:56:04 +0000

    feat: Add About app

---
+++ about/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class About(models.Model):
+    """
+    Stores About me text
+    """
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
