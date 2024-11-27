14997f8f1f473c7f5fb9f84dc1a957cff45b6cc6
Ruth De Swart <ruthdeswart@outlook.ie>
2024-11-27 12:54:52 +0000

    feat: Create view for post detail

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import BlogPost
 
 # Create your views here.
 class BlogDetail(generic.ListView):
+    """
+    Renders Blog enteries in jumbotron view
+    """
     queryset = BlogPost.objects.all()
     template_name = "blog/index.html"
     paginate_by = 6
+
+
+def post_detail(request, slug):
+    """
+    Renders the view for each blog post entry and its 
+    content based on blogs slug.
+    """
+
+    queryset = BlogPost.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post},
+    )
