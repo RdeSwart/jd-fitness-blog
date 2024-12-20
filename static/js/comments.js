6d2263a448a80e2d723350e342e2c4643d3f897c
Ruth De Swart <ruthdeswart@outlook.ie>
2024-12-20 21:21:27 +0000

    feat: Add Scroll-to-Top button on blog post_detail

--- static/js/comments.js
+++ static/js/comments.js
@@ -45,3 +45,14 @@ for (let button of deleteButtons) {
       deleteModal.show();
     });
   }
+
+  
+// Scroll to Top Button on post_detail page
+// Get the element
+let topBtn = document.querySelector(".top-btn");
+
+// On Click, Scroll to the page's top, replace 'smooth' with 'instant' if you don't want smooth scrolling
+topBtn.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
+
+// On scroll, Show/Hide the btn with animation
+window.onscroll = () => topBtn.style.opacity = window.scrollY > 500 ? 1 : 0;
