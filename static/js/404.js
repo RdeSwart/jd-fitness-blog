bc847bb40700f74a146991ef42a8d2247aed169d
Ruth De Swart <ruthdeswart@outlook.ie>
2024-12-21 20:24:07 +0000

    feat: Create 404 page

---
+++ static/js/404.js
@@ -0,0 +1,13 @@
+var svgContainer = document.getElementById('svgContainer');
+
+var animItem = bodymovin.loadAnimation({
+
+wrapper: svgContainer,
+
+animType: 'svg',
+
+loop: true,
+
+animationData: JSON.parse(animationData)
+
+});
